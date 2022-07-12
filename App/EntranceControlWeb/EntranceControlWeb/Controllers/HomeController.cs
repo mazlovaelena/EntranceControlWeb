8d8b74568e40371c272ba79101b1654fe67977cc
mazlovaelena <90679521+mazlovaelena@users.noreply.github.com>
2022-07-12 17:54:18 +0500

    Создание проекта, работа с дизайном

---
+++ App/EntranceControlWeb/EntranceControlWeb/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using EntranceControlWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntranceControlWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult HomePage()
+        {
+            return View();
+        }
+
+        public IActionResult Entrance()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
