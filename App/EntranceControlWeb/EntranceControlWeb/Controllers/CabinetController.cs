114fcfa847b3eb3940d3a1911e8573614f6bb74c
mazlovaelena <90679521+mazlovaelena@users.noreply.github.com>
2022-08-01 19:18:46 +0600

    Создание личного кабинета

---
+++ App/EntranceControlWeb/EntranceControlWeb/Controllers/CabinetController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using EntranceControlWeb.Models;
+
+namespace EntranceControlWeb.Controllers
+{
+    public class CabinetController : Controller
+    {
+        private EntranceControlWebContext _context;
+
+        public CabinetController (EntranceControlWebContext context)
+        {
+            
+            _context = context;
+        }
+
+        public IActionResult UserCab()
+        {
+            return View();
+        }
+        public IActionResult AdminCab()
+        {
+            return View();
+        }
+        public IActionResult Users()
+        {
+            return View();
+        }
+        public IActionResult Chart()
+        {
+            return View();
+        }
+    }
+}
